5efc87b86c0435bedd8bb57d3edad7302b3bfd4b
haeguri <haegyun821@gmail.com>
2021-11-07 12:51:46 +0900

    feat: nav 스타일 작업

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,30 @@
+import { Link } from "gatsby";
 import React from "react";
 
 const Layout: React.FC = ({ children }) => {
   return (
     <>
-      <nav>
-        <ul>about</ul>
+      <nav className="flex w-full h-16">
+        <div className="flex container mx-auto">
+          <Link to="/">Devlog</Link>
+          <ul className="flex ">
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/">Posts</Link>
+            </li>
+          </ul>
+        </div>
       </nav>
       <main>{children}</main>
+      <footer>
+        ©{" "}
+        <a href="https://github.com/haeguri" target="_blank">
+          haeguri
+        </a>
+        , Built with Gatsby
+      </footer>
     </>
   );
 };
